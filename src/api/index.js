d52f101098afc40935b8d10c063b16b6eb418ed8
Dmitry Belyaev <blv.dmitry@gmail.com>
2019-12-07 21:51:13 +0100

    API fixes

--- src/api/index.js
+++ src/api/index.js
@@ -4,15 +4,15 @@ const endpoints = {
   accounts: [accounts[1], accounts[2]],
 };
 
+const wait = async (timeout) => {
+  await new Promise(resolve => setTimeout(resolve, timeout));
+};
+
 export const fetchEndpoint = async (name) => {
-  const timeout = Math.floor(Math.random() * 2000);
-  let response;
+  const timeout = Math.floor(Math.random() * 1000);
 
-  await setTimeout(() => {
-    if (!endpoints[name]) throw Error ('Invalid endpoint');
+  if (!endpoints[name]) throw Error ('Invalid endpoint');
 
-    response = endpoints[name];
-  }, timeout);
-
-  return response;
+  await wait(timeout);
+  return endpoints[name];
 };
